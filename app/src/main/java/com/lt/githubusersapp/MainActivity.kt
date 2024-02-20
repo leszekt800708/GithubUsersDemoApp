e2984139acc126966a3477a368e0db76f2c3e001
leszek <leszek@bigchangedev.com>
2024-02-20 13:36:20 +0100

    fix import

--- app/src/main/java/com/lt/githubusersapp/MainActivity.kt
+++ app/src/main/java/com/lt/githubusersapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
